7ca7f1da32a5c7ecc31161fac4953c2da761343b
rominacuadra <romicuadra@gmail.com>
2024-10-16 19:32:57 -0300

    Add a bookController and his root

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class BookController extends Controller {
+    public function index(): View {
+        return view('books.index');
+    }
+}
