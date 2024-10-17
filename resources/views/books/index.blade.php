311e249a638c0db4abd723ab261cec0434d2d7a4
rominacuadra <romicuadra@gmail.com>
2024-10-16 22:53:05 -0300

    Adding a header section.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,3 +1,29 @@
 <x-layout>
-    Track your books!!
+    <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+        <img src="https://collegeinfogeek.com/wp-content/uploads/2018/11/Essential-Books.jpg" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
+{{--        <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">--}}
+        <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">
+            <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+        </div>
+        <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">
+            <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+        </div>
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:mx-0">
+                <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Admiring Our Readings, A Journey to Remember</h2>
+                <p class="mt-6 text-lg leading-8 text-white">
+                    Opening a book is like crossing into a new universe, one you never imagined. Books set us free, letting us explore unknown lands, live countless lives, and feel every emotion.
+                </p>
+                <p class="mt-6 text-lg leading-8 text-white">
+                    Reading is more than a pastime; it is a journey within, helping us understand the world and ourselves. Each story holds a revelation, a truth, a shared dream. Within the pages of every book lies the echo of those who dreamed of discovering something beyond the known.
+                </p>
+                <p class="mt-6 text-lg leading-8 text-white">
+                    So, come in. Read. Let yourself be transformed.
+                </p>
+                <h3 class="font-bold text-white text-3xl tracking-tight">
+                    "Reading is the most intimate of adventures, one that transcends the traveler."
+                </h3>
+            </div>
+        </div>
+    </div>
 </x-layout>
